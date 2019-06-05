56678b4e384a23a329c19cbfc499142e4323683a
PKrause79 <patrick.krause@qda-solutions.com>
2019-06-05 23:07:41 +0200

    updated handler to version 1

--- handler.js
+++ handler.js
@@ -1,9 +1,12 @@
+/********************************************
+ * Project:     "Bespaße die Katze" - Amazon Echo Alexa Skill
+ * Description: Backend handler (for Amazon AWS Serverless Lambda function) 
+ * GitHub:      https://github.com/PKrause79/amazon-echo-katze
+ * Author:      Patrick Krause / (c) 2019
+ *******************************************/
+
 /* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
-/**
- * Alexa Skill by Patrick Krause / 2017
- * based on https://github.com/alexa/skill-sample-nodejs-fact
- **/
 
 'use strict';
 
@@ -12,70 +15,70 @@ const Alexa = require('alexa-sdk');
 const APP_ID = 'amzn1.ask.skill.09a5ea7f-b897-49cc-8b3a-6087ac63d926';  
 
 const languageStrings = {
-    'en-GB': {
-        translation: {
-            FACTS: [
-                'Cat, come here!!',
-                'Miau, miau, miauuuuuuu?',
-                'Where is the bird? There! Catch the bird!',
-                'Bsssssssssssssssss. Bssssssssssssssssssss.',
-                'Miau <break time="3s"/> miauuuuuuuu <break time="3s"/> brrrrrrrrrr <break time="3s"/> tztztz.',
-                'piep.                           piep.                      piep',
-                'Cat! bssss, such a nice cat!',
-                'Catch the mouse! There! There is the mouse!',
-                'Faß! Faß! Los, faß!',
-				'Wanna eat? Cat, come eat!',
-				'Wanna cat sugar? So nice ...',
-            ],
-            SKILL_NAME: 'British the cat',
-            GET_FACT_MESSAGE: "",
-            HELP_MESSAGE: 'You can say call the cat, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    },
-    'en-US': {
-        translation: {
-            FACTS: [
-                'Cat, come here!!',
-                'Miau, miau, miauuuuuuu?',
-                'Where is the bird? There! Catch the bird!',
-                'Bsssssssssssssssssss. Bssssssssssssssssssss.',
-                'Miau <break time="3s"/> miauuuuuuuu <break time="3s"/> brrrrrrrrrr <break time="3s"/> tztztz.',
-                'piep.                           piep.                      piep',
-                'Cat! bssss, such a nice cat!',
-                'Catch the mouse! There! There is the mouse!',
-                'Faß! Faß! Los, faß!',
-				'Wanna eat? Cat, come eat!',
-				'Wanna cat sugar? So nice ...',
-            ],
-            SKILL_NAME: 'American the cat',
-            GET_FACT_MESSAGE: "",
-            HELP_MESSAGE: 'You can say call the cat, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    },
+    // 'en-GB': {
+    //     translation: {
+    //         FACTS: [
+    //             'Cat, come here!!',
+    //             'Miau, miau, miauuuuuuu?',
+    //             'Where is the bird? There! Catch the bird!',
+    //             'Bsssssssssssssssss. Bssssssssssssssssssss.',
+    //             'Miau <break time="3s"/> miauuuuuuuu <break time="3s"/> brrrrrrrrrr <break time="3s"/> tztztz.',
+    //             'piep.                           piep.                      piep',
+    //             'Cat! bssss, such a nice cat!',
+    //             'Catch the mouse! There! There is the mouse!',
+    //             'Faß! Faß! Los, faß!',
+				// 'Wanna eat? Cat, come eat!',
+				// 'Wanna cat sugar? So nice ...',
+    //         ],
+    //         SKILL_NAME: 'British the cat',
+    //         GET_FACT_MESSAGE: "",
+    //         HELP_MESSAGE: 'You can say call the cat, or, you can say exit... What can I help you with?',
+    //         HELP_REPROMPT: 'What can I help you with?',
+    //         STOP_MESSAGE: 'Goodbye!',
+    //     },
+    // },
+    // 'en-US': {
+    //     translation: {
+    //         FACTS: [
+    //             'Cat, come here!!',
+    //             'Miau, miau, miauuuuuuu?',
+    //             'Where is the bird? There! Catch the bird!',
+    //             'Bsssssssssssssssssss. Bssssssssssssssssssss.',
+    //             'Miau <break time="3s"/> miauuuuuuuu <break time="3s"/> brrrrrrrrrr <break time="3s"/> tztztz.',
+    //             'piep.                           piep.                      piep',
+    //             'Cat! bssss, such a nice cat!',
+    //             'Catch the mouse! There! There is the mouse!',
+    //             'Faß! Faß! Los, faß!',
+				// 'Wanna eat? Cat, come eat!',
+				// 'Wanna cat sugar? So nice ...',
+    //         ],
+    //         SKILL_NAME: 'American the cat',
+    //         GET_FACT_MESSAGE: "",
+    //         HELP_MESSAGE: 'You can say call the cat, or, you can say exit... What can I help you with?',
+    //         HELP_REPROMPT: 'What can I help you with?',
+    //         STOP_MESSAGE: 'Goodbye!',
+    //     },
+    // },
     'de-DE': {
         translation: {
-            FACTS: [
-                'Katze, komm her! Wie heißt Du denn? Bist Du ein feiner? Nein? Ich meinte, bist Du ein Schreiner? Hahaha! lass Dich nicht verarschen.',
-                'Miau, miau, miauuuuuuu? Hallo, aufgepasst! Katze? Du scheinst nicht sehr schlau zu sein!',
-                'Wo ist der Piepmatz? Kuck mal! Da ist er! Los, lauf hinterher! Nein? Ach ja stimmt, Du bist ja schließlich kein Hund, hahaha!',
-                'Bssssssssssssssss. Bssssssssssssssssssss.',
-                'Miau <break time="3s"/> miauuuuuuuu <break time="3s"/> brrrrrrrrrr <break time="3s"/> tztztz.',
-                'piep.                           piep.                      piep - wo ist das Mäuschen? Fang die Maus, los! Mach schon! Nein? Na los jetzt!',
-                'Katze! bssss, ja so ein feiner! Lass uns spielen beweg Dich nicht, also los: beweg Dich ab jetzt nicht für 5 Sekunden!',
-                'Hol die Maus! Da! Da ist die Maus, such! Aber bring sie bitte nicht mit ins Bett! Und am besten auch nicht ins Wohnzimmer! Am besten bringst Du sie gar nicht erst mit ins Haus, dann kriegst Du 100 Punkte! OK?',
-                'Faß! Faß! Los, faß! Beiß, fass, mach sie fertig! Nein? Dann bist Du eine gute Katze - wenn Du die anderen Tiere nicht quälst! Fein hast Du das gemacht!',
-				'Essen? Katze, ESSEN! Hahaha das war nur ein lustiger Witz - zum Glück bist Du nicht drauf reingefallen!',
-				'Willst Du ein Bonbon? Hmmm? Oder ein Leckerli? Kriegst Du aber nicht, hahaha!',
+            SENTENCES: [
+                '<break time="2s"/>Katze, komm her! <break time="2s"/> Wie heißt Du denn? <emphasis level="strong">Bist Du ein feiner?</emphasis>  Nein? <amazon:effect name="whispered">Ich meinte, bist Du ein Schreiner?</amazon:effect> Ha ha! <voice name="Hans"><emphasis level="strong">HEY! Lass die Katze in Ruhe!</emphasis></voice>',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_meow_1x_01" /> <break time="3s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_angry_meow_1x_01" />Katze? Was war denn das! Eine Nachbarskatze? <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_angry_screech_1x_01" /> ',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_bird_chickadee_chirps_01" /> Wo ist der Piepmatz? <break time="1s"/><emphasis level="strong">Kuck mal!</emphasis> Hier drin ist er! <break time="1s"/> <amazon:effect name="whispered">Los, lauf hinterher!</amazon:effect> <break time="1s"/>Nein? Katze, was stimmt mit Dir nicht? Ach ja, Du bist ja schließlich kein <emphasis level="moderate">Hund</emphasis>, ha ha ha!',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_dog_med_bark_2x_01" /> <audio src="soundbank://soundlibrary/animals/amzn_sfx_dog_med_bark_1x_01" /> <voice name="Hans"> <break time="1s"/> Ich bin ein fürchterlicher Piranja Pudel und Du solltest Dich vor mir fürchten! </voice> ',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_rat_squeak_2x_01" /> <break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_rat_squeaks_01"/> <break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_rat_squeak_2x_01" />',
+                '<break time="2s"/><emphasis level="strong"><prosody pitch="x-high">piep.<break time="3s"/>piep.<break time="2s"/>piep </prosody></emphasis> <break time="1s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_crow_caw_1x_01" /> <break time="1s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_crow_caw_1x_01" /><break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_crow_caw_1x_02"/> <amazon:effect name="whispered">wo ist das Vögelchen?</amazon:effect> <break time="2s"/> FANG DEN VOGEL, JETZT! <break time="2s"/> Nein? Na dann nicht!',
+                '<break time="2s"/>Katze! Ja so ein feiner! Lass uns "beweg Dich nicht" spielen, OK?<break time="1s"/>! Also los: beweg Dich ab jetzt nicht für 5 Sekunden!',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_rat_squeaks_01"/><break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_rat_squeak_2x_01" /> Hol die Maus! Da! Da ist die Maus, such! Aber bring sie bitte nicht mit ins Bett! Und am besten auch nicht ins Wohnzimmer! Am besten bringst Du sie gar nicht erst mit ins Haus, dann kriegst Du 100 Punkte! OK?',
+                '<break time="2s"/><amazon:effect name="whispered">Katze guck mal, da ist eine Maus!</amazon:effect> <break time="2s"/>FASS! FASS! LOS, fass die Maus! Beiß sie, fass - mach sie fertig! <break time="3s"/>Nein? Was ist bloß los mit Dir? Naja, dann bist Du halt eine <emphasis level="strong">gute</emphasis> Katze - wenn Du die anderen Tiere nicht quälst! <amazon:effect name="whispered">Fein hast Du das gemacht!</amazon:effect>',
+				'<break time="2s"/><emphasis level="strong">Essen?</emphasis> <break time="2s"/> Katze, <emphasis level="strong">ESSEN?</emphasis> Ha ha ha, das war nur ein lustiger Witz - zum Glück bist Du ja nicht drauf reingefallen! Heute gibt es nichts zu essen.',
+				'<break time="2s"/><amazon:effect name="whispered">Katze, willst Du ein Bonbon?</amazon:effect> Hmmm? <amazon:effect name="whispered">Oder ein Leckerli?</amazon:effect> Kriegst Du aber nicht, ha ha ha!',
+                '<break time="2s"/><audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_purr_01" /> <break time="1s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_purr_03" /> <break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_purr_01" /> <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_purr_02" /> <break time="2s"/> <audio src="soundbank://soundlibrary/animals/amzn_sfx_cat_purr_meow_01" />'
             ],
-            SKILL_NAME: 'Sprich mit der Katze',
-            GET_FACT_MESSAGE: '',
-            HELP_MESSAGE: 'Du kannst sagen, „Sprich mit der Katze“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
+            SKILL_NAME: 'Bespaße die Katze',
+            HELP_MESSAGE: 'Du kannst sagen, „Bespaße die Katze“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
+            HELP_REPROMPT: 'Wie kann ich Dir helfen?',
+            STOP_MESSAGE: 'Miau miao - auf Wiedersehen!',
         },
     },
 };
@@ -88,15 +91,21 @@ const handlers = {
         this.emit('GetFact');
     },
     'GetFact': function () {
-        // Get a random cat call from the list
-        // Use this.t() to get corresponding language data
-        const catArr = this.t('FACTS');
-        const catIndex = Math.floor(Math.random() * catArr.length);
-        const randomCat = catArr[catIndex];
+        
+        // get a random cat call from the list
+        // use this.t() to get corresponding language data
+        const sentenceArray = this.t('SENTENCES');
+        const sentenceIndex = Math.floor(Math.random() * sentenceArray.length);
+        const sentence = sentenceArray[sentenceIndex];
 
         // Create speech output
-        const speechOutput = this.t('GET_FACT_MESSAGE') + randomCat;
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomCat);
+        this.emit(
+            ':tellWithCard', 
+            sentence, 
+            this.t('SKILL_NAME'), 
+            'Die Katze wurde gerade bespaßt und ist nun vermutlich sehr glücklich!'
+            ); 
+            
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
